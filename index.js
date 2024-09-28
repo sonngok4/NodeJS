a646b4256bf896ea697c3099c5d94155c3440483
chienpv <phamvanbk11@gmail.com>
2024-09-28 09:02:43 +0700

    demo express

--- index.js
+++ index.js
@@ -1,90 +1,177 @@
-import http from "node:http";
-import path from "node:path";
-import os from "node:os";
-import fs from "node:fs";
-import EventEmitter from "node:events";
+import express from "express";
+const app = express();
+const port = 3000;
+let workExperience = [
+  {
+    name: "Front End Developer",
+    start_date: "Jan 2015",
+    end_date: "Jan 2015",
+    description:
+      "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.",
+  },
+  {
+    name: "Back end Developer",
+    start_date: "Jan 2015",
+    end_date: "Jan 2015",
+    description:
+      "Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.",
+  },
+];
 
-const eventEmitter = new EventEmitter();
+function buildItem({ name, start_date, end_date, description }) {
+  return `<div class="w3-container">
+          <h5 class="w3-opacity"><b>${name}</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>${start_date} - <span class="w3-tag w3-teal w3-round">${end_date}</span></h6>
+          <p>${description}</p>
+          <hr>
+        </div>`;
+}
 
-eventEmitter.on("end", (number, x) => {
-  console.log(`done ${number} ${x} !!!!!`);
+function buildExperien() {
+  let data = ``;
+
+  workExperience.forEach((element) => {
+    data += buildItem(element);
+  });
+  return data;
+}
+
+let html = `<!DOCTYPE html>
+<html>
+<head>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="https://www.w3schools.com/w3images/avatar_hat.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2>Jane Doe</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Designer</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>London, UK</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>ex@mail.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>1224435534</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+          <p>Adobe Photoshop</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:90%">90%</div>
+          </div>
+          <p>Photography</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:80%">
+              <div class="w3-center w3-text-white">80%</div>
+            </div>
+          </div>
+          <p>Illustrator</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:75%">75%</div>
+          </div>
+          <p>Media</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:50%">50%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>Spanish</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:55%"></div>
+          </div>
+          <p>German</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%"></div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+        ${buildExperien()}
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>W3Schools.com</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
+          <p>Web Development! All I need to know in one place</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>London Business School</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
+          <p>Master Degree</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>School of Coding</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+          <p>Bachelor Degree</p><br>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+</body>
+</html>
+`;
+
+app.get("/", (req, res) => {
+  res.send(html);
 });
 
-eventEmitter.emit("end", 2, 3);
-
-// let data = fs.readFileSync("./hello.txt", "utf8");
-// console.log(data);
-fs.writeFileSync("./hello.txt", "hello txt");
-http
-  .createServer((request, response) => {
-    response.write("<h1>Hello, Express!</h1>");
-    response.end();
-  })
-  .listen(8080);
-
-// function sum(a, b, fun) {
-//   let c = 0;
-//   setTimeout(() => {
-//     c = a + b;
-//     fun(c);
-//   }, 0);
-// }
-
-// let a = sum(1, 4, function (data) {
-//   console.log(data);
-// });
-
-function getUserID(fn) {
-  setTimeout(() => {
-    let userID = 10;
-    fn(userID);
-  });
-}
-
-function getPostUser(userID, fun) {
-  setTimeout(() => {
-    let post = { id: 12, name: "dfdfd" };
-    fun(post);
-  });
-}
-
-function getCommentPostUser(post, fun) {
-  setTimeout(() => {
-    let comment = { id: 13, name: "dfdfd" };
-    fun(comment);
-  });
-}
-
-// getUserID(function (userID) {
-//   getPostUser(userID, function (post) {
-//     getCommentPostUser(post, function (comment) {
-//       console.log(comment);
-//     });
-//   });
-// });
-
-let abc = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    getUserID((data) => {
-      resolve(data);
-    });
-  }, 0);
+app.listen(port, () => {
+  console.log("Server stated!!!");
 });
-
-abc
-  .then((data) => {
-    getPostUser(data, function (post) {
-      return post;
-    });
-  })
-  .then((post) => {
-    getCommentPostUser(post, function (comment) {
-      console.log(comment);
-    });
-  })
-  .catch((data) => {
-    console.log(data);
-  })
-  .finally(() => {
-    console.log("finally");
-  });
