f560467814852c860bc131d88adc663ed2d26fb6
chienpv <phamvanbk11@gmail.com>
2024-10-25 16:34:11 +0700

    demo query mongo

--- models/user.mjs
+++ models/user.mjs
@@ -2,10 +2,26 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const userScheme = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  workExperience: Array(Object),
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+    minLength: 6,
+  },
+  workExperience: {
+    type: Array(Object),
+  },
+  age: { type: Number },
+  role: {
+    type: String,
+    default: "User",
+    enum: ["Admin", "User"],
+  },
 });
 
 const User = mongoose.model("user", userScheme); //==> users
